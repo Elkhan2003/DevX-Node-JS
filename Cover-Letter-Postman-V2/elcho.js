dfa56f53430ceacb4772fbba07708ac4ca0bbed7
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2023-02-24 17:16:30 +0600

    Global Update

--- Cover-Letter-Postman-V2/elcho.js
+++ Cover-Letter-Postman-V2/elcho.js
@@ -4,20 +4,34 @@ const fs = require("fs");
 const app = express();
 
 app.get("/resume-and-download", (req, res) => {
-	const { jobTitle } = req.query;
-	const resumeName = `${jobTitle}-Resume.pdf`;
+	const { jobTitle, fileName } = req.query;
+	let resumeName;
 
-	fs.copyFile("./Frontend-Resume.pdf", `./${resumeName}`, (err) => {
+	if (jobTitle === "Frontend") {
+		resumeName = "Frontend-Resume.pdf";
+	} else if (jobTitle === "Backend") {
+		resumeName = "Backend-Resume.pdf";
+	} else if (jobTitle === "FullStack") {
+		resumeName = "FullStack-Resume.pdf";
+	} else {
+		res.status(400).send("Invalid jobTitle parameter");
+		return;
+	}
+
+	const finalFileName = fileName ? `${fileName}.pdf` : `${jobTitle}-Resume.pdf`;
+
+	fs.copyFile(`./${resumeName}`, `./${finalFileName}`, (err) => {
 		if (err) throw err;
-		console.log(`Resume copied and renamed to ${resumeName}`);
-		res.download(`./${resumeName}`, (err) => {
+		console.log(`Resume ${resumeName} copied and renamed to ${finalFileName}`);
+
+		res.download(`./${finalFileName}`, (err) => {
 			if (err) throw err;
-			console.log(`Resume ${resumeName} downloaded`);
+			console.log(`Resume ${finalFileName} downloaded`);
 		});
 	});
 });
 
 const port = 911;
 app.listen(port, () => {
-	console.log(`Server running on port By Elcho${port}`);
+	console.log(`Server running on port ${port}`);
 });
